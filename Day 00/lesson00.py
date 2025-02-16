b3996f4e76e06be8575a934c667d174d965cdb5b
chinvzhx <levanjajanidze@gmail.com>
2025-02-16 13:49:38 +0400

    day 00

---
+++ Day 00/lesson00.py
@@ -0,0 +1,86 @@
+from turtle import*
+
+
+# we want to paint a house
+
+# step 1:draw a square
+width(7)
+color("purple")
+begin_fill()
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+end_fill()
+# end of square
+# drawing a door
+
+forward(70)
+color("yellow")
+begin_fill()
+left(90)
+forward(120)
+right(90)
+forward(60)
+right(90)
+forward(120)
+end_fill()
+
+penup()
+goto(200, 200)
+pendown()
+
+color("red")
+begin_fill()
+right(150)
+forward(200)
+left(120)
+forward(200)
+end_fill()
+
+penup()
+goto(0, 0)
+pendown()
+
+color("purple")
+left(210)
+forward(70)
+left(270)
+forward(15)
+
+
+color("brown")
+forward(40)
+left(90)
+forward(50)
+left(90)
+forward(40)
+left(90)
+forward(50)
+
+penup()
+goto(200, 120)
+pendown()
+
+color("purple")
+right(90)
+forward(15)
+
+color("brown")
+forward(40)
+left(90)
+forward(50)
+left(90)
+forward(40)
+left(90)
+forward(50)
+
+
+exitonclick()
