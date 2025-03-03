91e20d8f920fd40bcb8cf7286d2f036607b3e70e
chinvhzx <levanjajanidze1999@gmail.com>
2025-03-03 23:52:10 +0400

    commit

---
+++ day 04/classwork/classwork.py
@@ -0,0 +1,16 @@
+# name= "levan"
+# age = 13
+# age = age + 1
+# name = name + "!"
+# print(age,name)
+
+   # aq vasworeb maswavlebis mocemul kods
+# name = Gurami
+# age = "15"
+# print("name " + age)
+# es aris araswori kodi
+
+# kodis gasworeba
+name = "gurami"
+age = 15
+print(name + str(age)) 
---
+++ day 04/homework/homework.py
@@ -0,0 +1,20 @@
+x= 15
+y=20
+z=25
+print(x,y,z)
+
+num1=15
+num2=19
+print(num1 + num2)
+
+name="kurt"
+lastname="cobain"
+print(name + lastname)
+
+name="vanheilen"
+print(name)
+name="Hetfield"
+print(name)
+# aq vasworebt shecdomas
+number = 5
+print("result" + str (number))
