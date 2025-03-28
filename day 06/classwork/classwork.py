4495e6f193a2e2469eedaef651123b5e9d0de964
chinvzhx <levanjajanidze1999@gmail.com>
2025-03-28 23:52:48 +0400

    second commit

--- day 06/classwork/classwork.py
+++ day 06/classwork/classwork.py
@@ -4,5 +4,20 @@
 
 
 # classwork N2
-name = input('please input ur name:')
-print('Hello' + name)
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+sum_result = number1 + number2
+difference_result = number1 - number2
+product_result = number1 * number2
+division_result = number1 / number2
+
+print(f"{number1} + {number2} = {sum_result}")
+print(f"{number1} - {number2} = {difference_result}")
+print(f"{number1} * {number2} = {product_result}")
+print(f"{number1} / {number2} = {division_result}")
+
+name = "Levan" 
+age = 13
+
+print("Hello {name}, you are {age} years old.")
