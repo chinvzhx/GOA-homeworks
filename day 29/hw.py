08fe86a88b63a4780404efcf6a28f1141a5f9923
chinvhzx <levanjajanidze1999@gmail.com>
2025-09-03 17:29:23 +0400

    commit

---
+++ day 29/hw.py
@@ -0,0 +1,35 @@
+# Level 29
+nums = [10, 20, 30, 40, 50, 60, 70]
+
+print(nums[:3])
+print(nums[-3:])
+print(nums[::-1])
+
+# Task 2
+word = "გოგონა"
+
+print(word[:4])
+print(word[3:])
+print(word[::2])
+
+# Task 3
+text = "სლაისინგი სასწაულია"
+
+print(text[:9])
+print(text[10:])
+print(text[::-2])
+
+# Task 4
+letters = ["ა", "ბ", "გ", "დ", "ე", "ვ", "ზ", "თ"]
+
+print(", ".join(letters[:3]))
+print(", ".join(letters[3:6]))
+print(", ".join(letters[6:]))
+print(", ".join(letters[::-1]))
+
+# Task 5
+sentence = "მასწავლებელი კარგია"
+
+print(sentence[:12])
+print(sentence[13:])
+print(sentence[:12:-1])
